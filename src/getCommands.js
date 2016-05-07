a935ecdd5e3254321d2020e799c64e83f458d1e2
Hassan Khan <contact@hassankhan.me>
2016-05-07 14:38:03 +0100

    General cleanup

--- src/getCommands.js
+++ src/getCommands.js
@@ -9,7 +9,7 @@ const findPlugins = require('./findPlugins');
  */
 module.exports = function getCommands() {
   const rsxRoot = process.env['RSX_PROJECT_ROOT'] = path.join(__dirname, '..');
-  const appRoot = process.env['RN_PROJECT_ROOT'] = process.cwd();
+  const appRoot = process.env.RN_PROJECT_ROOT = process.cwd();
 
   return uniq(
     flattenDeep([
