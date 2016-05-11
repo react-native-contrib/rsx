cdd617765a166fa0c4b17a636b5bb77471c67a96
Hassan Khan <contact@hassankhan.me>
2016-05-11 16:37:25 +0100

    Replaced Jest with Mocha/Chai/Sinon

---
+++ test/findPlugins.spec.js
@@ -0,0 +1,46 @@
+const chai = require('chai');
+const mock = require('mock-require');
+const path = require('path');
+
+const expect = chai.expect;
+
+const pjsonPath = path.join(process.cwd(), 'package.json');
+
+describe('findPlugins', () => {
+
+    it('should return an array of dependencies', () => {
+        mock(pjsonPath, {
+            dependencies: { 'rsx-plugin-test': '*' },
+        });
+        expect(findPlugins([process.cwd()])).to.be.an('array');
+    });
+
+    it('should return an empty array if there\'re no plugins in this folder', () => {
+        mock(pjsonPath, {});
+        expect(findPlugins([process.cwd()])).to.be.empty;
+    });
+
+    it('should return an empty array if there\'s no package.json in the supplied folder', () => {
+        expect(findPlugins(['fake-path'])).to.be.an('array');
+        expect(findPlugins(['fake-path'])).to.be.empty;
+    });
+
+    it('should return plugins from both dependencies and dev dependencies', () => {
+        mock(pjsonPath, {
+            dependencies: { 'rsx-plugin-test': '*' },
+            devDependencies: { 'rsx-plugin-test-2': '*' },
+        });
+        expect(findPlugins([process.cwd()]).length).to.equals(2);
+    });
+
+    it('should return unique list of plugins', () => {
+        mock(pjsonPath, {
+            dependencies: { 'rsx-plugin-test': '*' },
+            devDependencies: { 'rsx-plugin-test': '*' },
+        });
+        expect(findPlugins([process.cwd()]).length).to.equals(1);
+    });
+
+    afterEach(mock.stopAll);
+
+});
--- test/getCommands.spec.js
+++ test/getCommands.spec.js
@@ -1,49 +1,46 @@
-jest.autoMockOff();
-
 const path = require('path');
+const expect = require('chai').expect;
+const getCommands = require('../src/getCommands');
 const mock = require('mock-require');
+const mockFs = require('mock-fs');
+const sinon = require('sinon');
 const rewire = require('rewire');
 
 const commands = require('./fixtures/commands');
-const isArray = (arg) =>
-  Object.prototype.toString.call(arg) === '[object Array]';
 
 /**
- * Paths to two possible `node_modules` locations `preact` can be installed
+ * Paths to two possible `node_modules` locations RSX can be installed
  */
 const LOCAL_NODE_MODULES = path.join(process.cwd(), 'node_modules');
 const GLOBAL_NODE_MODULES = '/usr/local/lib/node_modules';
 
 /**
- * Paths to `package.json` of project, and preact - in two installation locations
+ * Paths to `package.json` of project, and RSX - in two installation locations
  */
 const APP_JSON = path.join(process.cwd(), 'package.json');
-const GLOBAL_PREACT_PJSON = path.join(GLOBAL_NODE_MODULES, 'preact', 'package.json');
-const LOCAL_PREACT_PJSON = path.join(LOCAL_NODE_MODULES, 'preact', 'package.json');
+const GLOBAL_RSX_PJSON = path.join(GLOBAL_NODE_MODULES, '/rsx/package.json');
+const LOCAL_RSX_PJSON = path.join(LOCAL_NODE_MODULES, 'rsx/package.json');
 
 /**
- * Sample `preact` plugin used in test cases
+ * Sample RSX plugin used in test cases
  */
-const SAMPLE_PREACT_PLUGIN = 'preact-plugin-test';
+const SAMPLE_RSX_PLUGIN = 'rsx-plugin-test';
 
 /**
- * Sample `package.json` of Preact that will be used in test cases
+ * Sample `package.json` of RSX that will be used in test cases
  */
-const SAMPLE_PREACT_JSON = {
+const SAMPLE_RSX_JSON = {
   dependencies: {
-    [SAMPLE_PREACT_PLUGIN]: '*',
+    [SAMPLE_RSX_PLUGIN]: '*',
   },
 };
 
 /**
- * Project without `preact` plugins defined
+ * Project without RSX plugins defined
  */
 const NO_PLUGINS_JSON = {
   dependencies: {},
 };
-
-const getCommands = rewire('../src/getCommands');
-var revert;
 
 describe('getCommands', () => {
 
@@ -51,121 +48,137 @@ describe('getCommands', () => {
 
   describe('in all installations', () => {
 
+    var getCommands;
+    var revert;
+
+    before(() => {
+      getCommands = rewire('../src/getCommands');
+      revert = getCommands.__set__({__dirname: path.join(LOCAL_NODE_MODULES, 'rsx/src')});
+    });
+
+    /**
+     * In this suite we only test RSX package.json, thus we make sure
+     * that project json on `process.cwd()` is properly mocked.
+     */
     beforeEach(() => {
-      revert = getCommands.__set__({
-        __dirname: path.join(LOCAL_NODE_MODULES, 'preact/src'),
-      });
       mock(APP_JSON, NO_PLUGINS_JSON);
     });
 
-    afterEach(() => revert());
+    after(() => revert());
 
     it('list of the commands should be a non-empty array', () => {
-      mock(APP_JSON, NO_PLUGINS_JSON);
-      mock(LOCAL_PREACT_PJSON, SAMPLE_PREACT_JSON);
-      mock(SAMPLE_PREACT_PLUGIN, commands.single);
+      mock(LOCAL_RSX_PJSON, SAMPLE_RSX_JSON);
+      mock(SAMPLE_RSX_PLUGIN, commands.single);
 
-      expect(getCommands().length).not.toBe(0);
-      expect(isArray(getCommands())).toBeTruthy();
+      expect(getCommands()).to.be.not.empty;
+      expect(getCommands()).to.be.an('array');
     });
 
     it('should export one command', () => {
-      mock(LOCAL_PREACT_PJSON, SAMPLE_PREACT_JSON);
-      mock(SAMPLE_PREACT_PLUGIN, commands.single);
+      mock(LOCAL_RSX_PJSON, SAMPLE_RSX_JSON);
+      mock(SAMPLE_RSX_PLUGIN, commands.single);
 
-      expect(getCommands().length).toEqual(1);
+      expect(getCommands().length).to.be.equal(1);
     });
 
     it('should export multiple commands', () => {
-      mock(LOCAL_PREACT_PJSON, SAMPLE_PREACT_JSON);
-      mock(SAMPLE_PREACT_PLUGIN, commands.multiple);
+      mock(LOCAL_RSX_PJSON, SAMPLE_RSX_JSON);
+      mock(SAMPLE_RSX_PLUGIN, commands.multiple);
 
-      expect(getCommands().length).toEqual(2);
+      expect(getCommands().length).to.be.equal(2);
     });
 
     it('should export unique list of commands by name', () => {
-      mock(LOCAL_PREACT_PJSON, {
+      mock(LOCAL_RSX_PJSON, {
         dependencies: {
-          [SAMPLE_PREACT_PLUGIN]: '*',
-          [`${SAMPLE_PREACT_PLUGIN}-2`]: '*',
+          [SAMPLE_RSX_PLUGIN]: '*',
+          [`${SAMPLE_RSX_PLUGIN}-2`]: '*',
         },
       });
 
-      mock(SAMPLE_PREACT_PLUGIN, commands.single);
-      mock(`${SAMPLE_PREACT_PLUGIN}-2`, commands.single);
+      mock(SAMPLE_RSX_PLUGIN, commands.single);
+      mock(`${SAMPLE_RSX_PLUGIN}-2`, commands.single);
 
-      expect(getCommands().length).toEqual(1);
+      expect(getCommands().length).to.be.equal(1);
     });
 
   });
 
   describe('project plugins', () => {
+
+    var getCommands;
+    var revert;
+
+    before(() => {
+      getCommands = rewire('../src/getCommands');
+    });
+
     /**
      * In this test suite we only test project plugins thus we make sure
-     * `preact` package.json is properly mocked
+     * RSX `package.json` is properly mocked.
      */
     beforeEach(() => {
-      mock(LOCAL_PREACT_PJSON, NO_PLUGINS_JSON);
-      mock(GLOBAL_PREACT_PJSON, NO_PLUGINS_JSON);
+      mock(LOCAL_RSX_PJSON, NO_PLUGINS_JSON);
+      mock(GLOBAL_RSX_PJSON, NO_PLUGINS_JSON);
     });
 
     afterEach(() => revert());
 
     it('shoud load when installed locally', () => {
-      revert = getCommands.__set__({
-        __dirname: path.join(LOCAL_NODE_MODULES, 'preact/src'),
-      });
+      revert = getCommands.__set__({__dirname: path.join(LOCAL_NODE_MODULES, 'rsx/src')});
 
-      mock(APP_JSON, SAMPLE_PREACT_JSON);
+      mock(APP_JSON, SAMPLE_RSX_JSON);
       mock(
-        path.join(LOCAL_NODE_MODULES, SAMPLE_PREACT_PLUGIN),
+        path.join(LOCAL_NODE_MODULES, SAMPLE_RSX_PLUGIN),
         commands.single
       );
 
-      expect(getCommands()[0]).toEqual(commands.single);
+      expect(getCommands()[0]).to.be.equal(commands.single);
     });
 
     it('should load when installed globally', () => {
-      revert = getCommands.__set__({
-        __dirname: path.join(GLOBAL_NODE_MODULES, 'preact/src'),
-      });
+      revert = getCommands.__set__({__dirname: path.join(GLOBAL_NODE_MODULES, 'rsx/src')});
 
-      mock(APP_JSON, SAMPLE_PREACT_JSON);
+      mock(APP_JSON, SAMPLE_RSX_JSON);
       mock(
-        path.join(LOCAL_NODE_MODULES, SAMPLE_PREACT_PLUGIN),
+        path.join(LOCAL_NODE_MODULES, SAMPLE_RSX_PLUGIN),
         commands.single
       );
 
-      expect(getCommands()[0]).toEqual(commands.single);
+      expect(getCommands()[0]).to.be.equal(commands.single);
     });
 
   });
 
-  describe('preact and project plugins', () => {
+  describe('rsx and project plugins', () => {
 
-    beforeEach(() => {
-      revert = getCommands.__set__({
-        __dirname: path.join(LOCAL_NODE_MODULES, 'preact/src'),
-      });
+    var getCommands;
+    var revert;
+
+    before(() => {
+      getCommands = rewire('../src/getCommands');
+      revert = getCommands.__set__({__dirname: path.join(LOCAL_NODE_MODULES, 'rsx/src')});
     });
 
-    afterEach(() => revert());
+    after(() => revert());
 
     it('should load concatenated list of plugins', () => {
-      mock(APP_JSON, SAMPLE_PREACT_JSON);
-      mock(LOCAL_PREACT_PJSON, {
+      mock(APP_JSON, SAMPLE_RSX_JSON);
+      mock(LOCAL_RSX_PJSON, {
         dependencies: {
-          [`${SAMPLE_PREACT_PLUGIN}-2`]: '*',
+          [`${SAMPLE_RSX_PLUGIN}-2`]: '*',
         },
       });
 
       mock(
-        path.join(LOCAL_NODE_MODULES, SAMPLE_PREACT_PLUGIN),
+        path.join(LOCAL_NODE_MODULES, SAMPLE_RSX_PLUGIN),
         commands.multiple
       );
-      mock(`${SAMPLE_PREACT_PLUGIN}-2`, commands.single);
+      mock(`${SAMPLE_RSX_PLUGIN}-2`, commands.single);
 
-      expect(getCommands().length).toEqual(3);
+      expect(getCommands().length).to.be.equal(3);
     });
+
   });
+
 });
